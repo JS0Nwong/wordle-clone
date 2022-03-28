179fdb1c11698cf663f6990a4c865e58351e1589
Jason <55802733+JS0Nwong@users.noreply.github.com>
2022-03-28 00:51:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,110 +1,75 @@
-//KEY PRESS EVENTS
+const targetWords = [];
+const dictionary = [];
 
-const keys = [
-    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'enter',
-    'z', 'x', 'c', 'v', 'b', 'n', 'm', '<i class="fas fa-backspace"></i>'
-];
+const guessGrid = document.querySelector('[data-guess-grid]');
 
-//CREATE TILES THAT APPEAR ON THE SCREEN
-const guessRows = [
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-]
+startInteraction();
 
-let currentRow = 0;
-let currentTile = 0;
-let isGameOver = false;
-
-guessRows.forEach((guessRows, rowIndex) => {
-    const rowElement = document.createElement('div');
-    rowElement.classList.add('row');
-    rowElement.classList.add('guess-row')
-    rowElement.setAttribute('id', 'guessRow-' + rowIndex);
-
-    guessRows.forEach((guessRow, tileIndex) => {
-        const tile = document.createElement('div');
-        tile.setAttribute('id', 'guessRow-' + rowIndex + '-tile-' + tileIndex);
-        tile.classList.add('box');
-        rowElement.append(tile);
-    });
-    guess.append(rowElement);
-});
-
-//KEYBOARD EVENT LISTENER
-
-document.addEventListener('keydown', (e) => {
-    if(e.key.match(/^[a-z]+$/i))
-    {
-        addLetter(e.key);
-        console.log(e.key);
-    }
-    if(e.key === 'Backspace')
-    {
-        deleteLetter();
-    }
-});
-
-let key = document.querySelectorAll('.key');
-
-for(let i = 0; i < key.length; i++)
+function startInteraction()
 {
-    key[i].addEventListener('click', (e) => {
-        console.log(e.target.innerHTML);
-        handleClick(key[i].innerHTML);
-    });
+    document.addEventListener("click", handleMouseClick());
+    document.addEventListener("keydown", handleKeyPress());
 }
 
-const handleClick = (key) => {
-    if(!isGameOver)
-    {
-        if(key === '<i class="fas fa-backspace"></i>')
-        {
-            deleteLetter();
-            return;
-        }
-        if(key === 'enter')
-        {
-            checkRow();
-            return;
-        }
-        addLetter(key);
-    }
-}
-
-const addLetter = (key) =>
+function stopInteraction()
 {
-    if(currentTile < 5 && currentRow < 6)
+    document.removeEventListener("click", handleMouseClick());
+    document.removeEventListener("keydown", handleKeyPress());
+}
+
+function handleMouseClick(e)
+{
+    if(e.target.matches("[data-key]"))
     {
-        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
-        tile.textContent = key;
-        guessRows[currentRow][currentTile] = key;
-        tile.setAttribute('data', key);
-        currentTile++;
+        pressKey(e.target.dataset.key);
+        return;
     }
-}
-
-const deleteLetter = () => {
-    if(currentTile > 0)
-    {   
-        currentTile--;
-        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
-        tile.textContent = '';
-        guessRows[currentRow][currentTile] = '';
-        tile.setAttribute('data', '');
-    }
-}
-
-const checkRow = () => {
-    const guess = guessRows[currentRow].join('');
-
-    if(currentTile > 4)
+    if(e.target.matches("[data-enter]"))
     {
-        
+        submitGuess();
+        return;
+    }
+    if(e.target.matches("[data-delete]"))
+    {
+        deleteKey();
+        return;
     }
 }
 
+function handleKeyPress(e)
+{
+    console.log(e);
+    if(e.key === "Enter")
+    {
+        submitGuess();
+        return;
+    }
+    if(e.key === "Backspace" || e.key === "Delete")
+    {
+        deleteKey();
+        return;
+    }
+    if(e.key.match(/^[a-z]$/))
+    {
+        pressKey(e.key);
+        return;
+    }
+}
+
+function pressKey(key)
+{
+    const nextTile = guessGrid.querySelector(":not([data-letter])");
+    nextTile.dataset.letter = key.toLowerCase();
+    nextTile.textContent = key;
+    nextTile.dataset.state = "active";
+}
+
+function submitGuess()
+{
+
+}
+
+function deleteKey()
+{
+
+}
